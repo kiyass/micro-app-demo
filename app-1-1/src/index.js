c4ad24fe92d99cbf6d40dc588f8e125e9fc63a3c
XS367 <xuanjiaqi@harmonycloud.cn>
2024-09-09 09:50:32 +0800

    fix: onmount and onunmont

--- app-1-1/src/index.js
+++ app-1-1/src/index.js
@@ -1,35 +1,35 @@
 // import './public-path';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 // import 'antd/dist/antd.css';
 
 // 发送数据
-window.microApp?.dispatch({'from': '来自微应用react17的数据' + (+new Date())})
+window.microApp?.dispatch({ from: "来自微应用react17的数据" + +new Date() });
 
 // 👇 将渲染操作放入 mount 函数，子应用初始化时会自动执行
-window.mount = () => {
+window.onmount = () => {
   ReactDOM.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById("root")
   );
 
   console.log("微应用react17渲染来了 -- UMD模式");
-}
+};
 
 // 👇 将卸载操作放入 unmount 函数
-window.unmount = () => {
+window.onunmount = () => {
   ReactDOM.unmountComponentAtNode(document.getElementById("root"));
   console.log("微应用react17卸载了 -- UMD模式");
-}
+};
 
 // 如果不在微前端环境，则直接执行mount渲染
 if (!window.__MICRO_APP_ENVIRONMENT__) {
-  window.mount()
+  window.onmount();
 }
 
 // If you want to start measuring performance in your app, pass a function
